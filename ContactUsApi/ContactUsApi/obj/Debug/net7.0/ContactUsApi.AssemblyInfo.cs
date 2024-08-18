25fdea6db5611a1272c44b0ba8f6877f9686c014
marian <112700576+marianyuzu@users.noreply.github.com>
2024-08-19 06:46:30 +1000

    add README file and setup docker

--- ContactUsApi/ContactUsApi/obj/Debug/net7.0/ContactUsApi.AssemblyInfo.cs
+++ ContactUsApi/ContactUsApi/obj/Debug/net7.0/ContactUsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactUsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e7b52dee633d588a86cd56710877db716e91c73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactUsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactUsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
